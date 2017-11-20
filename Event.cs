338e30dcb5f525bc146552e0e896db6f2de77088
Hussein Reda <33789990+Hussein-Reda@users.noreply.github.com>
2017-11-20 20:06:18 +0200

    Add files via upload

---
+++ Event.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS101
+{
+    class Event
+    {
+        private int id;
+        private DateTime date;
+        private string name;
+        private string decription;
+
+        public Event(int id, DateTime date, string name, string decription)
+        {
+            this.id = id;
+            this.date = date;
+            this.name = name;
+            this.decription = decription;
+        }
+
+        public override string ToString()
+        {
+            return id+"/"+date+"/"+name+"/"+decription;
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS101
+{
+    public partial class Form1 : Form
+    {
+        private EventsFeed feed;
+        private FileStream fs;
+        public Form1()
+        {
+            InitializeComponent();
+            feed = new EventsFeed();
+            fs= new FileStream("cs.txt", FileMode.Append, FileAccess.Write);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Event ev=new Event(0,dateTimePicker1.Value,textBox2.Text,textBox3.Text);
+            if (feed.addEvent(ev))
+            {
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine(ev.ToString());
+                sw.Close();
+                label1.Text = "Added";
+            }
+            else
+            {
+                label1.Text = "Something Wrong";
+            }
+            
+        }
+    }
+}
